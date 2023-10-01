6857a7cc5d69a1b150923124a7da345e74db2f5d
Ahmed Hussein <wolktheline@yahoo.com>
2023-10-01 13:43:17 -0600

    Creating a new window using Tk() class and keep the window open ny using mainloop() method.

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 import tkinter
 
 window = tkinter.Tk()
+window.title("GUI program")
+window.minsize(width=300, height=500)
+
+
+# Label
+my_label = tkinter.Label(text="I'm a label")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 window.mainloop()
