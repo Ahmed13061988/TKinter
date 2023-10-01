73b8bf50c8bf1dee5c06a2beb10da6e67bf9bf59
Ahmed Hussein <wolktheline@yahoo.com>
2023-10-01 14:12:19 -0600

    Making a function takes different number of arguments without specifying it in the parameters.

--- main.py
+++ main.py
@@ -9,12 +9,17 @@ my_label = tk.Label(text="I'm a Label", font=("Arial", 24))
 my_label.pack(side="left")
 
 
-def my_function(a=1, b=4, c=10):
-    return a + b * c
+# def my_function(a=1, b=4, c=10):
+#     return a + b * c
+#
+#
+# result = 0
+# result = my_function()
+# print(result)
 
-
-result = 0
-result = my_function()
-print(result)
+def add(*arg):
+    for i in arg:
+        print(i)
+add(1,2,3,4,5,6,7,"a")
 
 window.mainloop()
