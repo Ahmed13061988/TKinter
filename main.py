9fc52b61137af9277194481f580a40906fae3569
Ahmed Hussein <wolktheline@yahoo.com>
2023-10-01 14:05:55 -0600

    Advanced arguments in Python.

--- main.py
+++ main.py
@@ -4,33 +4,17 @@ window = tk.Tk()
 window.title("GUI program")
 window.minsize(width=500, height=300)
 
-
 # Label
-my_label = tk.Label(text="I'm a label")
-my_label.pack()
+my_label = tk.Label(text="I'm a Label", font=("Arial", 24))
+my_label.pack(side="left")
 
 
+def my_function(a=1, b=4, c=10):
+    return a + b * c
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+result = 0
+result = my_function()
+print(result)
 
 window.mainloop()
