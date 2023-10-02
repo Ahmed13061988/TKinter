0da1a876ae229532939924e8ace85aa5abf0b4cb
Ahmed Hussein <wolktheline@yahoo.com>
2023-10-02 11:13:10 -0600

    Using grid() to place the widgets on the window.

--- main.py
+++ main.py
@@ -15,15 +15,16 @@ my_label = tk.Label(text="I'm a Label", font=("Arial", 24))
 my_label.config(text="Press")
 
 # Button
-
+new_botton = tk.Button()
 
 # Entry
 
 input = tk.Entry(width=10)
 
 button = tk.Button(text="Click me!", command=button_clicked)
-my_label.grid(column=1, row=1)
-button.grid(column=2,row=2)
-input.grid(column=3,row=3)
+my_label.grid(column=0, row=0)
+button.grid(column=2, row=2)
+new_botton.grid(column=3, row=0)
+input.grid(column=4, row=4)
 
 window.mainloop()
